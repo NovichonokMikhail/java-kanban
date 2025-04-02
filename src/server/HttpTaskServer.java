5d945b7be709551ba8562df82cf789eda36dc628
Mike Novichonok <mo.novichonok@gmail.com>
2025-04-02 10:24:50 -0400

    fix: обновил код для соответсвия CodeStyle #4

--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -15,18 +15,15 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class HttpTaskServer {
-    private final int PORT = 8080;
-    private final TaskManager manager;
     private final HttpServer server;
 
     public HttpTaskServer(TaskManager manager) throws IOException {
-        server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/tasks", new TasksHandler(manager));
         server.createContext("/epic", new EpicsHandler(manager));
         server.createContext("/subtasks", new SubtasksHandler(manager));
         server.createContext("/history", new HistoryHandler(manager));
         server.createContext("/prioritized", new PrioritizedHandler(manager));
-        this.manager = manager;
     }
 
     public static void main(String[] args) throws IOException {
